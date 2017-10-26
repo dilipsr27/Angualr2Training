f52e90dadbf91d6e358d18b26e19858cd63ae3f6
Riyaz Hawaldar <riyaz.hawaldar@lrn.com>
2017-10-26 15:59:46 +0530

    home component created

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'home',
@@ -10,6 +11,19 @@ export class HomeComponent{
   private name:string;
   private location:string;
   private today:Date;
+
+  private colors:string[]=['red','green','blue'];
+  private products:Product[]=[
+  {id:101,name:'Book',price:1250,mfgDate:new Date(2016,1,8)},
+  {id:102,name:'Pen',price:1200,mfgDate:new Date()},
+  {id:103,name:'Coca Cola',price:1000,mfgDate:new Date()},
+  {id:104,name:'Pepsi',price:90,mfgDate:new Date(2016,5,6)},
+  {id:105,name:'Thumbs Up',price:80,mfgDate:new Date()}
+];
+
+private currentYear:number = new Date().getFullYear();
+
+private count:number=3;
   
   constructor() {
       this.name = "Riyaz Hawaldar";
---
+++ src/app/models/product.ts
@@ -0,0 +1,6 @@
+export class Product{
+    public id:number;
+    public name:string;
+    public mfgDate:Date;
+    public price:number;
+}
