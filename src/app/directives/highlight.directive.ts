c21b9237da0469ea494a538eaaa9ccf02637f739
Riyaz Hawaldar <riyaz.hawaldar@lrn.com>
2017-10-27 12:25:09 +0530

    use of Renderer service

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,16 +1,17 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
 export class HighlightDirective {
   @Input('highlight') public myColorName:string //@Input() public highlight:string - if variable name and att name is different then we have to explicitly pass the attr name inside @input()
-  constructor(private el:ElementRef) { // Dependency Injection - private el becomes a member variable
+  constructor(private el:ElementRef, private renderer:Renderer) { // Dependency Injection - private el becomes a member variable
      
    }
 
    ngOnInit(){
-      this.el.nativeElement.style.backgroundColor=this.myColorName;
+     // this.el.nativeElement.style.backgroundColor=this.myColorName;
+     this.renderer.setElementStyle(this.el.nativeElement,"backgroundColor",this.myColorName);
    }
    
 }
