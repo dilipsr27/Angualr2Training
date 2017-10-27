b49a9e533836eb94baa8548e4ba07040f024b1f4
Riyaz Hawaldar <riyaz.hawaldar@lrn.com>
2017-10-27 12:14:34 +0530

    custom directive with custom values

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,16 +1,16 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
 export class HighlightDirective {
-
+  @Input('highlight') public myColorName:string //@Input() public highlight:string - if variable name and att name is different then we have to explicitly pass the attr name inside @input()
   constructor(private el:ElementRef) { // Dependency Injection - private el becomes a member variable
-      this.doSomething();
+     
    }
 
-   doSomething(){
-      this.el.nativeElement.style.backgroundColor="yellow";
+   ngOnInit(){
+      this.el.nativeElement.style.backgroundColor=this.myColorName;
    }
-
+   
 }
